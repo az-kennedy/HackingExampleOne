e44caec513070c3e8e5204fdd1493f4bc7436ac1
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-18 21:28:49 -0500

    Updated code with semi collins

--- action.php
+++ action.php
@@ -1,13 +1,9 @@
 <?php
 
-if ($_POST['username']) {
-  echo "Hello World"
+if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
+  echo "Success!  You hacked the website, nice job!";
+  echo "The next exercise is located at: URLHERE";
+} else {
+  echo "Wrong username and password. Try again";
 }
-
-#if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
-#  echo "Success!  You hacked the website, nice job!"
-#  echo "The next exercise is located at: URLHERE"
-#} else {
-#  echo "Wrong Username and Password. Try again"
-#}
 ?>
