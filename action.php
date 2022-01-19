8a4309a79941bcb47098ff3afc8bb4f71b1e827d
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-18 21:15:28 -0500

    Updated action.php to simplify the code for testing

--- action.php
+++ action.php
@@ -1,18 +1,13 @@
-#<?php
-#if ($a > $b) {
-#    echo "a is bigger than b";
-#} elseif ($a == $b) {
-#    echo "a is equal to b";
-#} else {
-#    echo "a is smaller than b";
-#}
-#?>
-
 <?php
-if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
-  echo "Success!  You hacked the website, nice job!"
-  echo "The next exercise is located at: URLHERE"
-} else {
-  echo "Wrong Username and Password. Try again"
+
+if ($_POST['username']) {
+  echo "Hello World"
 }
+
+#if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
+#  echo "Success!  You hacked the website, nice job!"
+#  echo "The next exercise is located at: URLHERE"
+#} else {
+#  echo "Wrong Username and Password. Try again"
+#}
 ?>
