e94807c968f4d7bb5baec80cd01563d8c1cf75c8
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-19 07:11:29 -0500

    fixed http - changed to https

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php
 
-if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
+if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1!") {
   echo "Success!  You hacked the website, nice job!";
   echo "The next exercise is located at: URLHERE";
 } else {
