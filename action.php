54529c5c92bec90fa487ec1df1d47e5818c7f1f1
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-18 21:00:38 -0500

    Added some php to process login page

--- action.php
+++ action.php
@@ -1,2 +1,18 @@
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo (int)$_POST['age']; ?> years old.
+#<?php
+#if ($a > $b) {
+#    echo "a is bigger than b";
+#} elseif ($a == $b) {
+#    echo "a is equal to b";
+#} else {
+#    echo "a is smaller than b";
+#}
+#?>
+
+<?php
+if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1") {
+  echo "Success!  You hacked the website, nice job!"
+  echo "The next exercise is located at: URLHERE"
+} else {
+  echo "Wrong Username and Password. Try again"
+}
+?>
