aca00e3e1effbce1526814c64ae81fc6a2af43df
Thomas Kennedy <thomaskennedy1066@gmail.com>
2022-03-16 07:42:16 -0400

    added new line in html

--- action.php
+++ action.php
@@ -2,8 +2,11 @@
 
 if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1!") {
   echo "Success!  You hacked the website, nice job! \r\n \r\n";
+  echo "<br>";
+  echo "<br>";
   echo "The next exercise is located at this URL: [https://webapp-ndclhackingexercisestwo.azurewebsites.net] \r\n \r\n";
-  echo "";
+  echo "<br>";
+  echo "<br>";
   echo "Hint: Use SQL Injection";
 } else {
   echo "Wrong username and password. Try again";
