c41f0d90d1ccaeb7cadce76bbf1eb06d1ac61397
Thomas Kennedy <thomaskennedy1066@gmail.com>
2022-03-15 20:56:04 -0400

    SQL injection hint

--- action.php
+++ action.php
@@ -3,6 +3,8 @@
 if (($_POST['username']) == "User1" && ($_POST['password']) == "Password1!") {
   echo "Success!  You hacked the website, nice job!";
   echo "The next exercise is located at: URLHERE";
+  echo ""
+  echo "Hint: Use SQL Injection";
 } else {
   echo "Wrong username and password. Try again";
 }
